502f72262d38ee330da7813a6b4585efdfb8f4f6
Takihamu <yukiko0802black@gmail.com>
2021-10-12 12:28:31 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def index
@@ -12,17 +13,39 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+    @book = Book.new(book_params)
+    if @book.save
+    flash[:success] = 'Book was successfully created'
+    redirect_to book_path(@book.id)
+   else
+    @books = Book.all
+     render "index"
+  end
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:success] = 'Book was successfully updated'
+      redirect_to book_path(@book.id)
+    else
+      render "edit"
+    end
+  end
+
+   def destroy
+     book = Book.find(params[:id])
+     book.destroy
+     flash[:success] = 'Book was successfully destroyed.'
+     redirect_to books_path
   end
 
   private
   def book_params
     params.require(:book).permit(:title,:body)
   end
-
-end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root :to => 'homes#top'
+  get 'info/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
-
   resources :books
+  get 'books' => 'books#show'
 
   post 'books' => 'books#create'
 end
